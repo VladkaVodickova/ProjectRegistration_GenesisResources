7bbd527664b17bbe1908ce77822b9964cac128e2
VV <vladkavodickova@gmail.com>
2023-08-04 20:35:21 +0200

    valid detail view

--- src/main/java/com/engeto/project2/Controller/MainController.java
+++ src/main/java/com/engeto/project2/Controller/MainController.java
@@ -5,29 +5,39 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.engeto.project2.UserInfo;
 import java.sql.*;
+import java.util.UUID;
 
 @RestController
 public class MainController {
 
     @GetMapping ("/api/v1/user/{id}")
-    public ResponseEntity<String> findUser(@PathVariable ("id") int userId){
+    public ResponseEntity<String> findUser(@PathVariable ("id") int userId, @RequestParam (value = "detail", defaultValue = "false") boolean detail){
         try (Connection con = getConnection())
              {
-            String query = ("SELECT ID, Name, Surname FROM registrationinfo WHERE ID = ?;");
-            PreparedStatement statement = con.prepareStatement(query);
+            String query;
+             if (detail) {
+                 query = "SELECT * FROM registrationinfo WHERE ID = ?;";
+             } else {
+                 query = "SELECT ID, Name, Surname FROM registrationinfo WHERE ID = ?;";
+             }
+             PreparedStatement statement = con.prepareStatement(query);
             statement.setInt(1, userId);
             ResultSet result = statement.executeQuery();
             if (result.next())  {
                 int id = result.getInt("ID");
                 String name = result.getString("Name");
                 String surname = result.getString("Surname");
-                UserInfo user = new UserInfo(id, name, surname);
+                UserInfo user;
+                if (detail) {
+                    String personId = result.getString("PersonId");
+                    UUID uuid = UUID.fromString(result.getString("Uuid"));
+                    user = new UserInfo(id, name, surname, personId, uuid);
+                } else {
+                    user = new UserInfo(id, name, surname);
+                }
                 try {
                     return ResponseEntity.ok(convertToJson(user));
                 } catch (JsonProcessingException e) {
@@ -41,6 +51,8 @@ public class MainController {
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred");
     }
+
+
 
     @PostMapping ("api/v1/user")
     public String userCreation( ){
