f1695f10bfba476476533aa7004dc36d9102d21c
VV <vladkavodickova@gmail.com>
2023-08-04 19:30:51 +0200

    added method getAll in JSON

--- src/main/java/com/engeto/project2/Controller/MainController.java
+++ src/main/java/com/engeto/project2/Controller/MainController.java
@@ -1,37 +1,50 @@
 package com.engeto.project2.Controller;
 
-import com.engeto.project2.Service.UserRepository;
-import com.engeto.project2.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.catalina.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import com.engeto.project2.UserInfo;
+import java.sql.*;
 
 @RestController
 public class MainController {
-    private UserRepository userRepository;
 
-    @Autowired
-    public MainController(UserRepository userRepository){
-        this.userRepository = userRepository;
+    @GetMapping ("/api/v1/user/id")
+    public ResponseEntity<String> findUser(){
+        try (
+                Connection con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/Engeto_Project2_Registration",
+                        "root",
+                        "EngetoJavaHeslo123*")) {
+            Statement statement = con.createStatement();
+            statement.executeQuery("SELECT * FROM registrationinfo;");
+            ResultSet result = statement.getResultSet();
+            if (result.next())  {
+                int id = result.getInt("ID");
+                String name = result.getString("Name");
+                String surname = result.getString("Surname");
+                String personId = result.getString("PersonID");
+                UserInfo user = new UserInfo(id, name, surname, personId );
+                return ResponseEntity.ok(convertToJson(user));
+            }
+        } catch (SQLException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private String convertToJson(UserInfo user) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(user);
     }
 
-    @GetMapping ("/api/v1/user/{ID}")
-    public String findUser(@PathVariable int ID){
-        return "JSON";
-    }
 
     @PostMapping ("api/v1/user")
-    public String userCreation(@RequestBody String name, String surname, String personID){
-        return "User saved!" + name + surname + personID ;
-    }
-
-    @GetMapping ("/api/v1/user")
-    public List<User> getAll(){
-        return userRepository.selectAll();
+    public String userCreation( ){
+        return "UserInfo saved!";
     }
 
 
