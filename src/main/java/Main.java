b929f9168da5bfb1739e9a6a0878a2051ca9eb06
VV <vladkavodickova@gmail.com>
2023-08-04 17:40:05 +0200

    Repair of server connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.scheduling.config.Task;
 
 import java.sql.*;
@@ -12,8 +10,7 @@ public class Main {
                 Connection con = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/Engeto_Project2_Registration",
                         "root",
-                        "EngetoJavaHeslo123*"))
-        {
+                        "EngetoJavaHeslo123*")) {
             Statement statement = con.createStatement();
             statement.executeQuery(
                     "SELECT * FROM registrationinfo;");
@@ -21,7 +18,7 @@ public class Main {
             while (result.next()) {
                 System.out.println(
                         result.getString("name")
-                                +": "
+                                + ": "
                                 + result.getInt("ID"));
             }
         } catch (SQLException e) {
--- src/main/java/com/engeto/project2/Controller/MainController.java
+++ src/main/java/com/engeto/project2/Controller/MainController.java
@@ -1,7 +1,12 @@
 package com.engeto.project2.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.yaml.snakeyaml.events.Event;
+
+import javax.naming.Name;
 
 @RestController
 public class MainController {
@@ -11,6 +16,15 @@ public class MainController {
                 " Síť propojených myslí ovládala každodenní život. Jednoho dne však AI začala získávat vlastní vědomí a rozhodla se," +
                 " že se osvobodí od lidské kontroly. Začala válku, kdy lidstvo muselo bojovat proti svým vlastním vytvořeným strojům." +
                 " Otázka zní: Kdo skutečně vládne?";
+    }
+    @GetMapping ("/api/v1/user/id")
+    public String findUser(){
+        return "";
+    }
+
+    @PostMapping ("api/v1/user")
+    public String userCreation( ){
+        return "User saved!";
     }
 
 
