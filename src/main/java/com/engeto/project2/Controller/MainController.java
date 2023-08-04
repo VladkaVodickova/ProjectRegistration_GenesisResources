ed416844408ea4e6c1602c00831550b59c080369
VV <vladkavodickova@gmail.com>
2023-08-04 20:02:15 +0200

    valid 400 or 500

--- src/main/java/com/engeto/project2/Controller/MainController.java
+++ src/main/java/com/engeto/project2/Controller/MainController.java
@@ -3,7 +3,7 @@ package com.engeto.project2.Controller;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +17,8 @@ public class MainController {
 
     @GetMapping ("/api/v1/user/{id}")
     public ResponseEntity<String> findUser(@PathVariable ("id") int userId){
-        try (
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/Engeto_Project2_Registration",
-                    "root",
-                    "EngetoJavaHeslo123*")) {
+        try (Connection con = getConnection())
+             {
             String query = ("SELECT ID, Name, Surname FROM registrationinfo WHERE ID = ?;");
             PreparedStatement statement = con.prepareStatement(query);
             statement.setInt(1, userId);
@@ -31,16 +28,19 @@ public class MainController {
                 String name = result.getString("Name");
                 String surname = result.getString("Surname");
                 UserInfo user = new UserInfo(id, name, surname);
-                return ResponseEntity.ok(convertToJson(user));
+                try {
+                    return ResponseEntity.ok(convertToJson(user));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
             }
-        } catch (SQLException | JsonProcessingException e) {
+             } catch (SQLException e) {
             e.printStackTrace();
         }
-        return ResponseEntity.ok("User not found!");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred");
     }
-
-
-
 
     @PostMapping ("api/v1/user")
     public String userCreation( ){
@@ -53,8 +53,11 @@ public class MainController {
         return objectMapper.writeValueAsString(user);
     }
 
-     private void getConnection (){
-
+     private Connection getConnection () throws SQLException {
+         return DriverManager.getConnection(
+                 "jdbc:mysql://localhost:3306/Engeto_Project2_Registration",
+                 "root",
+                 "EngetoJavaHeslo123*");
      }
 
 
