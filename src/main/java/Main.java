ff1d1e3c1c1a665a569254a8fdcf8d01c506b098
VV <vladkavodickova@gmail.com>
2023-08-05 11:51:06 +0200

    updated hierarchy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,3 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
-
-import java.sql.*;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
--- src/main/java/com/engeto/project2/MockServer/Application.java
+++ src/main/java/com/engeto/project2/MockServer/Application.java
@@ -1,9 +1,11 @@
-package com.engeto.project2;
+package com.engeto.project2.MockServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.engeto.project2.controller")
 public class Application {
 
 	public static void main(String[] args) {
